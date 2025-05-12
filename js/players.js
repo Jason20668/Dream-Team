1e9404e9fbc8a186717bbab8bb40c0a55b6c3f8c
Jason20668 <526JBach@frhsd.com>
2025-05-12 10:42:29 -0400

    have

---
+++ js/players.js
@@ -0,0 +1,37 @@
+const players = [
+    {
+        firstName: 'John',
+        lastName: 'Walker',
+        Alias: 'US Agent',
+        Superpower: 'Enhanced strength, sight, hearing',
+        Photo: 'https://picsum.photos/200/300'
+    },
+    {
+        firstName: 'Alexei',
+        lastName: 'Shostakov',
+        Alias: 'Red Guardian',
+        Superpower: 'Enhanced strength, sight',
+        Photo: 'https://picsum.photos/200/300'
+    },
+    {
+        firstName: 'Bucky',
+        lastName: 'Barnes',
+        Alias: 'Winter Soldier',
+        Superpower: 'Metal Arm',
+        Photo: 'https://picsum.photos/200/300'
+    },
+    {
+        firstName: 'Bob',
+        lastName: 'Reynolds',
+        Alias: 'Sentry',
+        Superpower: 'Yes',
+        Photo: 'https://picsum.photos/200/300'
+    },
+    {
+        firstName: 'Danny',
+        lastName: 'Rand',
+        Alias: 'Iron Fist',
+        Superpower: 'Iron Fist powers',
+        Photo: 'https://picsum.photos/200/300'
+    }
+]
---
+++ js/roster.js
@@ -0,0 +1,59 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const grid = document.getElementById('rosterGrid')
+
+    const render = list => {
+        grid.innerHTML = ''
+        list.forEach(p => {
+            const col = document.createElement('div')
+            col.className = 'col-6 col-lg-2'
+
+            col.innerHTML = 
+            `
+                <div class="card h-100 shadow-sm">
+                    <img src="${p.Photo}" alt="${p.firstName} ${p.lastName}" class="card-img-top">
+                    <div class="card-body text-center">
+                        <h5 class="card-title mb-1">${p.firstName} ${p.lastName}</h5>
+                        <div class='badge badge-position badge-pos-${p.Alias}'>Alias: ${p.Alias}</div>
+                        <p class="small text-muted mb-0">Superpower: ${p.Superpower}</p>
+                    </div>
+                </div>
+            `
+            grid.appendChild(col)
+        })
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+render(players)
+})
